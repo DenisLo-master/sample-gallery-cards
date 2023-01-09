8751a33f35ff801fd289636054aeb0b1dd435e8f
DenisLo-master <66447463+DenisLo-master@users.noreply.github.com>
2023-01-10 03:13:48 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-const slides = document.querySelectorAll(".slide")
-const bodyBgImage = document.querySelector("body")
+function slidePlugin(activeSlide = 0) {
+  const slides = document.querySelectorAll(".slide")
+  const bodyBgImage = document.querySelector("body")
+  slides[activeSlide].classList.add("active");
 
-for (const slide of slides) {
-    slide.addEventListener("click", () => {
-        clearActiveClasses();
-        const bgImage = slide.style.backgroundImage
-        slide.classList.add("active");
-        bodyBgImage.style.backgroundImage = bgImage
-    })
+  for (const slide of slides) {
+      slide.addEventListener("click", () => {
+          clearActiveClasses();
+          const bgImage = slide.style.backgroundImage
+          slide.classList.add("active");
+          bodyBgImage.style.backgroundImage = bgImage
+      })
+  }
+
+  function clearActiveClasses() {
+      slides.forEach(slide => slide.classList.remove("active"));
+  }
 }
 
-function clearActiveClasses() {
-    slides.forEach(slide => slide.classList.remove("active"));
-}
+slidePlugin(2)
